01859fc612de9f44307811bbdac6b291f415cbf9
yoshiki <yoshiki0911@gmail.com>
2020-10-01 23:33:51 +0900

    errorの追加

---
+++ src/qiita_analysis/error.py
@@ -0,0 +1,12 @@
+
+
+class QiitaAnalysisBaseError(Exception):
+    pass
+
+
+class QiitaTokenInvalidError(QiitaAnalysisBaseError):
+    pass
+
+
+class QiitaGetItemError(QiitaAnalysisBaseError):
+    pass
